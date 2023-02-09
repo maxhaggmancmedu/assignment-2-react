be7855ee7200dd88efdbcf50de57dd64cdb03c76
Max Häggman <max.haggman@edu.cmeducations.se>
2023-02-09 13:39:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,135 @@
+import fs from 'fs/promises'
+import {formatDistanceToNow, isAfter, isBefore, parse, format, isToday, set} from 'date-fns'
+import {Command} from 'commander';
+import getGitVersion from './src/getGitVersion.js';
+
+const gitVersion = await getGitVersion();
+const first = 'Max';
+const last = 'Häggman';
+
+// process.env.npm_config_user_agent only works when using `npm run start` not `node index.js`
+const npmAndNode = process.env.npm_config_user_agent;
+const startOfCourse = new Date(2023, 0, 31)
+const daysSinceStartOfCourse = formatDistanceToNow(startOfCourse);
+const dateStringSentAsArgument = argumentParser.args[0]
+const dateSentAsArgument = parse(dateStringSentAsArgument, 'yyyy-MM-dd', new Date())
+const currentDate = set(new Date(), {hours: 0, minutes: 0, seconds: 0, milliseconds: 0})
+const currentDateAndTime = format(new Date(), 'yyyy-MM-dd HH:mm');
+
+const argumentParser = new Command();
+argumentParser.option('--date')
+argumentParser.parse();
+
+let datesCompared = '';
+
+function compareDates() {
+    if (isAfter(dateSentAsArgument, currentDate)) {
+        datesCompared = 'after' 
+        console.log(`The Entered date: "${dateStringSentAsArgument}" is after today's date`)
+    } else if (isBefore(dateSentAsArgument, currentDate)) {
+        datesCompared = 'before'
+        console.log(`The Entered date: "${dateStringSentAsArgument}" is before today's date`)
+    } else {
+        datesCompared = 'currently'
+        console.log(`The Entered date: "${dateStringSentAsArgument}" is today`)
+    }
+}
+
+compareDates();
+
+const fileContent = `
+name: ${first} ${last}
+npm & node: ${npmAndNode}
+git version: ${gitVersion}
+Days since start of course: ${daysSinceStartOfCourse}
+Today's date and time: ${currentDateAndTime}
+Entered date is ${dateStringSentAsArgument} and it is ${datesCompared} today's date!
+`;
+
+const htmlFile = `
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="index.css" />
+    	<title>Assignment 2</title>
+	</head>
+	<body>
+		<header class="header">
+			<h1>Assignment 2</h1>
+    	</header>
+    	<main class="content">
+			<div class="content-item">
+				Npm & Node version: <span class="results">${npmAndNode}</span>
+			</div>
+			<div class="content-item">
+			Git version: <span class="results">${gitVersion}</span>
+			</div>
+			<div class="content-item">
+				Days since start of course: <span class="results">${daysSinceStartOfCourse}</span>
+			</div>
+			<div class="content-item">
+				Today's date and time: <span class="results">${currentDateAndTime}</span>
+			</div>
+			<div class="content-item">
+				The entered date <span class="results">${dateStringSentAsArgument}</span> is <span class="results">${datesCompared}</span> today's date!
+			</div>
+    	</main>
+		
+</body>
+</html>
+`;
+
+const cssFile = `
+html {
+	font-size: 10px;
+	height: 100vh;
+  }
+  
+body {
+	background-color: #000000;
+    color: #fff;
+	font-family: sans-serif;
+	font-size: 1.4rem;
+	height: 100%;
+	line-height: 1.6;
+	margin: 0;
+}
+  
+ * {
+	box-sizing: border-box;
+}
+
+.header {
+	align-items: center;
+    border-bottom: 3px solid #fff;
+	display: flex;
+	height: 20vh;
+	justify-content: center;
+    margin: 0 96px;
+}
+
+.content {
+	height: 60vh;
+	margin: 0 auto;
+	max-width: 600px;
+	padding: 56px;
+}
+
+.content-item {
+	font-size: 20px;
+	margin-bottom: 12px;
+}
+
+.results {
+	font-weight: 700;
+}
+
+`;
+
+await fs.writeFile('index.md', fileContent);
+await fs.writeFile('index.html', htmlFile);
+await fs.writeFile('index.css', cssFile);
+
+
---
+++ index.md
@@ -0,0 +1,7 @@
+
+name: [45mMax[49m [43mHäggman[49m
+npm & node: npm/8.19.3 node/v18.13.0 win32 x64 workspaces/false
+git version: git version 2.38.1.windows.1
+Days since start of course: 10 days
+Today's date and time: 2023-02-09 13:25
+Entered date is 2023-02-08 and it is before today's date!
